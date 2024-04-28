ce9a0e27495e03765777157e12341461a58d92c5
SharmaRajan <r7870062870@gmail.com>
2024-04-28 06:43:28 +0530

    Added frontend app for DatingApp

--- API/Program.cs
+++ API/Program.cs
@@ -8,15 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
 
+// Add services to the container
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+// The middleware is very important
 // Configure the HTTP request pipeline.
+
+
+
 // if (app.Environment.IsDevelopment())
 // {
 //     app.UseSwagger();
@@ -25,35 +33,15 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+
 app.UseAuthorization();
+
+
 
 app.MapControllers();
 
-/*
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-*/
 
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'Dating app';
+
+  users: any;
+
+  baseUrl : string = "https://localhost:5001/api/users";
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get(this.baseUrl).subscribe({
+      next : response => this.users = response,
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
+
+}
